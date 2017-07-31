1df5e16952ef9b0d148a46bcd4c64bc9326f2715
haley <misshaleyn@gmail.com>
2017-07-31 11:47:08 -0400

    First steps, Wordpress

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * GimmeSubs functions and definitions
+ *
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 660;
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Decode
+ * @since Decode 1.0
+ */
+
+get_header(); ?>
